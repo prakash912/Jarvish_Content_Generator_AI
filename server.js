7ca368381d4e5bfdfb89672c6ccd7dea1b8afc34
prakash <rai530579@gmail.com>
2024-04-02 22:34:25 +0530

    connect mongoDB Atlas

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 import { Telegraf } from "telegraf";
+import userModel from "./src/models/User.js";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start(async (ctx) => {
   console.log(ctx.update.message.from, "ctx");
   console.log("hello i am jarvi");
-
-  //store user information into DB
-  await ctx.reply("hello i am jarvis");
+  const from = ctx.update.message.from;
+  try {
+    await userModel.findOneAndUpdate(
+      { tgId: from.id },
+      {
+        $setOnInsert: {
+          firstName: from.first_name,
+          lastName: from.last_name,
+          isBot: from.is_bot,
+          userName: from.username,
+        },
+      },
+      { upsert: true, new: true }
+    );
+    //store user information into DB
+    await ctx.reply(
+      `Hey ${from.first_name},Welcome. I will be writing highly engaging social media posts fro you. Just Keep feeding me with the events throught the day.Let's shine on social media.`
+    );
+  } catch (err) {
+    console.log(err, "err");
+    await ctx.reply("Facing Difficulties");
+  }
 });
 
 bot.launch();
