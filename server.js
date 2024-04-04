074e5c3ec7e52f849b11834d1ccce38ab28b75b5
prakash <rai530579@gmail.com>
2024-04-04 22:12:32 +0530

    add events model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { Telegraf } from "telegraf";
 import userModel from "./src/models/User.js";
+import eventModel from "./src/models/Event.js";
 import connectDb from "./src/config/db.js";
 import { message } from "telegraf/filters";
 
@@ -40,8 +41,50 @@ bot.start(async (ctx) => {
   }
 });
 
+bot.command("generate", async (ctx) => {
+  const from = ctx.update.message.from;
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+
+  //get the events for user
+  const events = await eventModel.find({
+    tgId: from.id,
+    createdAt: {
+      $gte: startOfDay,
+      $lte: endOfDay,
+    },
+  });
+
+  if (events.length === 0) {
+    await ctx.reply("No Events for the day");
+    return;
+  }
+
+  // make open api call
+  // store token count
+  // send response
+
+  await ctx.reply("Doing Things...");
+});
+
 bot.on(message("text"), async (ctx) => {
-  ctx.reply("Got the Message");
+  const from = ctx.update.message.from;
+  const message = ctx.update.message.text;
+  console.log(ctx);
+  try {
+    await eventModel.create({
+      text: message,
+      tgId: from.id,
+    });
+    ctx.reply(
+      "Noted, Keep texting me your thoughts.To generate the posts, just enter the command: /generate"
+    );
+  } catch (err) {
+    console.log(err);
+    await ctx.reply("Facing difficuties, please try again later.");
+  }
 });
 
 bot.launch();
